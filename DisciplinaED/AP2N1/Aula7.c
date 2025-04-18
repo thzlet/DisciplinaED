4e93e02c174090835f6a5207e5749b4a765ab71c
thzlet <leticiarosembergsousa@gmail.com>
2025-04-17 21:26:00 -0300

    Update file

--- DisciplinaED/AP2N1/Aula7.c
+++ DisciplinaED/AP2N1/Aula7.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float media(int n, float* v); /// prototipo da funçao media
+
 int main(void) {
     int tamanho;
 
@@ -9,13 +11,13 @@ int main(void) {
 
     float* vetor = (float*)malloc(tamanho * sizeof(float)); // alocando memoria para o vetor
 
-    if (vetor == NULL) { // retornando null se nao houver espaço para alocar
-        printf("Erro na alocação de memória.\n");
+    if (vetor == NULL) {
+        printf("Erro na alocação de memória.\n"); // retornando null se nao houver espaço para a alocar
         return 1;
     }
 
-    for (int i = 0; i < tamanho; i++) { // usuariio dando entrada nos valores de cada posicao
-        printf("Digite o valor para a posição %d: ", i);
+    for (int i = 0; i < tamanho; i++) {
+        printf("Digite o valor para a posição %d: ", i); // preenchendo o vetor
         scanf("%f", &vetor[i]);
     }
 
@@ -24,7 +26,18 @@ int main(void) {
         printf("vetor[%d] = %.2f\n", i, vetor[i]);
     }
 
-    free(vetor); // liberando memoria
+    float m = media(tamanho, vetor); // calculando a media
+    printf("\nA média dos valores é: %.2f\n", m);
+
+    free(vetor); // liberando a memoria
 
     return 0;
+}
+
+float media(int n, float* v) { // função que calcula media
+    float soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += v[i];
+    }
+    return soma / n;
 }
